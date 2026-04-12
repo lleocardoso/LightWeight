99f95f8a4c034d7dc30196a3edb706be328a2ab5
Leonardo Cardoso <lleocardoso@icloud.com>
2026-04-12 14:15:00 -0300

    chore: configuracao inicial do spring boot e estrutura do projeto

---
+++ src/main/java/com/LightWeight/LightWeightApplication.java
@@ -0,0 +1,12 @@
+package com.LightWeight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LightWeightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LightWeightApplication.class, args);
+    }
+}
